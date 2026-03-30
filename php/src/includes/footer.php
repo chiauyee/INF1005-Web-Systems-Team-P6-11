2ec3ea7e870c5e5bcb3a0e3ec2b6bb8628b4b700
saad <saadrraza@gmail.com>
2026-03-30 11:41:52 +0800

    footer contrast issue

--- php/src/includes/footer.php
+++ php/src/includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container text-center">
         <p><em>Copyright &copy; 2026 MusicMarket. All Rights Reserved.</em></p>
-        <p class='footer' class="mb-0"><a href="contact.php">Contact Us</a> | Cookie Policy | Terms of Service | Privacy Policy</p>
+        <p class='footer mb-0'><a href="contact.php">Contact Us</a> | Cookie Policy | Terms of Service | Privacy Policy</p>
     </div>
 </footer>
